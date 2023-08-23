c296b04c4191b156112016b4a500b4f41380ef5f
azagupta <116111865+azagupta@users.noreply.github.com>
2023-08-23 13:13:16 +0100

    Update iris.py

--- iris.py
+++ iris.py
@@ -41,3 +41,21 @@ def prediction(SepalLength, SepalWidth, PetalLength, PetalWidth):
     return "Iris-virginica"
   else:
     return "Iris-versicolor"
+
+# S10.3: Perform this activity in Sublime editor after adding the above code.
+# Add title widget
+st.title("Iris Flower Species Prediction App")
+
+# Add 4 sliders and store the value returned by them in 4 separate variables.
+s_length = st.slider("Sepal Length", 0.0, 10.0)
+s_width = st.slider("Sepal Width", 0.0, 10.0)
+p_length = st.slider("Petal Length", 0.0, 10.0)
+p_width = st.slider("Petal Width", 0.0, 10.0)
+
+# When 'Predict' button is pushed, the 'prediction()' function must be called
+# and the value returned by it must be stored in a variable, say 'species_type'.
+# Print the value of 'species_type' and 'score' variable using the 'st.write()' function.
+if st.button("Predict"):
+	species_type = prediction(s_length, s_width, p_length, p_width)
+	st.write("Species predicted:", species_type)
+	st.write("Accuracy score of this model is:", score)
