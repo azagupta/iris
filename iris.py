fc1d93e5eaa0cd534bcf80ee6aae7862b7280e67
azagupta <116111865+azagupta@users.noreply.github.com>
2023-08-23 13:09:08 +0100

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,43 @@
+# S10.1: Copy this code cell in 'iris_app.py' using the Sublime text editor. You have already created this ML model in the previous class(es).
+# Importing the necessary libraries.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+# Loading the dataset.
+iris_df = pd.read_csv("iris-species.csv")
+
+# Adding a column in the Iris DataFrame to resemble the non-numeric 'Species' column as numeric using 'map()' function.
+# Creating the numeric target column 'Label' to 'iris_df' using 'map()'.
+iris_df['Label'] = iris_df['Species'].map({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor':2})
+
+# Creating a model for Support Vector classification to classify the flower types into labels '0', '1', and '2'.
+
+# Creating features and target DataFrames.
+X = iris_df[['SepalLengthCm','SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
+y = iris_df['Label']
+
+# Spliting the data into training and testing sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+
+# Creating the SVC model and storing the accuracy score in a variable 'score'.
+svc_model = SVC(kernel = 'linear')
+svc_model.fit(X_train, y_train)
+score = svc_model.score(X_train, y_train)
+
+# S10.2: Perform this activity in Sublime editor after adding the above code.
+# Create a function 'prediction()' that accepts 'SepalLength', 'SepalWidth', 'PetalLength' and 'PetalWidth' as inputs and returns the species name.
+@st.cache()
+def prediction(SepalLength, SepalWidth, PetalLength, PetalWidth):
+  species = svc_model.predict([[SepalLength, SepalWidth, PetalLength, PetalWidth]])
+  species = species[0]
+  if species == 0:
+    return "Iris-setosa"
+  elif species == 1:
+    return "Iris-virginica"
+  else:
+    return "Iris-versicolor"
